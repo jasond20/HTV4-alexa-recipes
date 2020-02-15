bc7549f3140af3d38300fbf99aa8c81d23bf203b
nurgnij <36308139+nurgnij@users.noreply.github.com>
2020-02-15 16:42:14 -0500

    Update webscraping.py
    Added changes by @mingyeeee

--- webscraping.py
+++ webscraping.py
@@ -2,51 +2,94 @@ import requests
 import traceback
 from lxml import html
 
-link = 'https://www.allrecipes.com/recipe/273231/coconut-tres-leches-cake/?internalSource=similar_recipe_banner&referringId=275698&referringContentType=Recipe&clickId=simslot_1'
-pageContent=requests.get(link)
+def food_scraping(link):
+    #link = 'https://www.allrecipes.com/recipe/273524/blueberry-goat-cheese-and-basil-pie/?internalSource=similar_recipe_banner&referringId=270677&referringContentType=Recipe&clickId=simslot_2'
+    pageContent=requests.get(link)
+    tree = html.fromstring(pageContent.content)
 
-tree = html.fromstring(pageContent.content)
-i =1
-contentAvailible = True
+    i = 1
+    ingredients = []
+    ingredientsXpath = ['//*[@id="lst_ingredients_1"]/li[%i]/label/span/text()',
+                        '//*[@id="lst_ingredients_2"]/li[%i]/label/span/text()']
+    xPathCounter = 0
+    ingredientsListNum = 1
+    steps = []
 
-ingredients = []
-steps = []
-
-#read steps
-while(True):
-    try:
-        xPath = '//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ol/li[%x]/span/text()'%i
-        i += 1
-        step = tree.xpath(xPath)
-
-        if len(step) == 0:
+    #read steps
+    while(True):
+        try:
+            xPath = '//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ol/li[%x]/span/text()'%i
+            i += 1
+            step = tree.xpath(xPath)
+            step = str(step)[2:len(str(step))-32]
+            if len(step) == 0:
+                break
+            steps.append(step)
+            print(step)
+        except:
+            traceback.print_exc()
             break
-        steps.append(step)
-        print(step)
+    i = 1
+    #ingredients list
+    while(True):
+        try:
+            xPath = ingredientsXpath[xPathCounter]%i
 
-    except:
-        traceback.print_exc()
-        break
-i =1
-#ingredients list
-while(True):
-    try:
-        xPath = '// *[ @ id = "lst_ingredients_1"] / li[%x] / label / span / text()'%i
-        i+=1
-        ingredient = tree.xpath(xPath)
-        if len(ingredient) == 0:
+            i += 1
+            #print(xPath)
+            ingredient = tree.xpath(xPath)
+            ingredient =str(ingredient)[2:len(str(ingredient))-2]
+            if ingredient == "[\'Add all ingredients to the list\']":
+                break
+            if len(ingredient) == 0:
+                xPathCounter+=1
+                i =1
+                if(xPathCounter>1):
+                    break
+            ingredients.append(ingredient)
+            print(ingredient)
+
+        except:
+            traceback.print_exc()
             break
-        ingredients.append(ingredient)
-        print(ingredient)
 
-    except:
-        break
+    prepTime = tree.xpath('//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ul/li[2]/time/span/span/text()')
+    prepTime = str(prepTime)[2:len(str(prepTime))-2]
+    print(prepTime)
 
-prepTime = tree.xpath('//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ul/li[2]/time/span/span/text()')
-print(prepTime)
+    prepTimeUnit = tree.xpath('//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ul/li[2]/time/span/text()')
+    print(prepTimeUnit)
 
-cookTime = tree.xpath('//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ul/li[3]/time/span/span/text()')
-print(cookTime)
+    cookTime = tree.xpath('//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ul/li[3]/time/span/span/text()')
+    print(cookTime)
 
-readyIn = tree.xpath('//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ul/li[4]/time/span/span/text()')
-print(readyIn)
+    cookTimeUnit = tree.xpath('//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ul/li[3]/time/span/text()')
+    print(cookTimeUnit)
+
+    readyIn = tree.xpath('//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ul/li[4]/time/span/span/text()')
+    print(readyIn)
+
+    readyInTimeUnit = tree.xpath('//*[@id="main-content"]/div[3]/section/section[2]/div/div[1]/ul/li[4]/time/span/text()')
+    print(readyInTimeUnit)
+
+def food_search(food):
+    link = 'https://www.allrecipes.com/search/results/?wt=%s&sort=re'%food
+    pageContent=requests.get(link)
+    tree = html.fromstring(pageContent.content)
+
+    option[1][0] = tree.xpath('//*[@id="fixedGridSection"]/article[2]/div[2]/h3//a/@href')
+    option[1][1] = tree.xpath('//*[@id="fixedGridSection"]/article[5]/div[2]/h3//a/@href')
+    option[1][2] = tree.xpath('//*[@id="fixedGridSection"]/article[6]/div[2]/h3//a/@href')
+
+    option[2][0] = tree.xpath('//*[@id="fixedGridSection"]/article[2]/div[2]/h3/a/span/text()')
+    option[2][1] = tree.xpath('//*[@id="fixedGridSection"]/article[5]/div[2]/h3/a/span/text()')
+    option[2][2] = tree.xpath('//*[@id="fixedGridSection"]/article[6]/div[2]/h3/a/span/text()')
+
+    print(option[1][0])
+    print(option[1][1])
+    print(option[1][2])
+
+    return option
+
+options = food_search('cake')
+food_scraping(options[1][2])
